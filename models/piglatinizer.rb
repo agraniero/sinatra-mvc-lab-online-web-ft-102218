596608143d02f93b4dc6931adb6021818006eb15
Anthony Graniero <ananfro@gmail.com>
2019-01-30 18:40:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,5 +2,11 @@ class PigLatinizer
   
   def piglatinize(string)
     @string = string.split(" ")
+    
+    @string.map do |word|
+      if word.upcase.start_with?('A','E','I','O','U')
+        word << 'way'
+      else 
+    
   
 end
